1e124aa655401024e9629da71f69b1ab49d3b8a0
martin avila <106031819+martinUwUx@users.noreply.github.com>
2025-01-27 20:08:01 -0300

    Cambios iniciales

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'https://zelthoriaismp.cloud/nebula/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://zelthoriaismp.cloud/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://breakcoreproyectmc.b-cdn.net/servers/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
